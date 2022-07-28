14f29d000ed61ec71d324af15f4f9fd47f164a23
Olivia Adongo <akinyioliviatove@gmail.com>
2022-07-28 22:32:49 +0300

    Router added to the server

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
+
+// dependencies initialized
+
+const router = require('./router');

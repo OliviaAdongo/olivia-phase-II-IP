a7fb3318b278467de3b6c0b0f06f08504ba8715c
Olivia Adongo <akinyioliviatove@gmail.com>
2022-07-30 08:18:13 +0300

    Json file added

---
+++ client/src/components/Contacts/Contacts.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from 'axios';
+
+
+function Contacts (){
+    
+    
+    return (
+        <div>
+         
+        </div>
+    )
+
+
+}
+export default Contacts;
